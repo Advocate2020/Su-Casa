3125d9950a5ff2c3f4ce38c015443182f239432b
Sakhanya Swartbooi <swartbooisakhanya@gmail.com>
2024-09-22 19:27:28 +0200

    add breeze authentication

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('avatar')->default('/uploads/blank-profile-picture.png');
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('role', ['user', 'admin'])->defualt('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
